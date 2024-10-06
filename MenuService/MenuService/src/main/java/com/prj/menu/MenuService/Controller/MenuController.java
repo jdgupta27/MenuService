a68de017f65488a0c7dd70c9c0d6de836221c9eb
jd.gupta27 <jd.gupta27@gmail.com>
2024-10-06 23:03:50 +0530

    Add graphql code

--- MenuService/MenuService/src/main/java/com/prj/menu/MenuService/Controller/MenuController.java
+++ MenuService/MenuService/src/main/java/com/prj/menu/MenuService/Controller/MenuController.java
@@ -3,6 +3,9 @@ package com.prj.menu.MenuService.Controller;
 import com.prj.menu.MenuService.Entity.Item;
 import com.prj.menu.MenuService.Service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,32 +18,42 @@ public class MenuController {
 
     @Autowired MenuService menuService;
 
-    @PostMapping
-    public ResponseEntity<Item> createHotel(@RequestBody Item item){
-        Item item1 = menuService.addItem(item);
+    @MutationMapping
+    public ResponseEntity<Item> addItem(@Argument String name, @Argument String description, @Argument Double price, @Argument String catagory, @Argument Boolean available){
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        item.setPrice(price);
+        item.setCatagory(catagory);
+        item.setAvailable(available);
+        menuService.addItem(item);
         return ResponseEntity.status(HttpStatus.CREATED).body(item);
     }
 
-    @GetMapping("/")
+    @QueryMapping(name = "getItems")
     public ResponseEntity<List<Item>> getAllItems(){
         List<Item> allItems = menuService.getAllItems();
         return ResponseEntity.ok(allItems);
     }
 
-    @GetMapping("/{itemId}")
-    public ResponseEntity<Item> getItem(@PathVariable Long itemId){
+    @QueryMapping()
+    public ResponseEntity<Item> getItem(@Argument Long itemId){
         Item item = menuService.getItem(itemId);
         return ResponseEntity.ok(item);
     }
 
-    @PutMapping("/")
-    public ResponseEntity<Item> updateItem(@RequestBody Item item){
-        Item item1 = menuService.updateItem(item);
-        return ResponseEntity.ok(item1);
+    @MutationMapping("/")
+    public ResponseEntity<Item> updateItem(@Argument Long itemId, @Argument Double price, @Argument Boolean available){
+        Item item = new Item();
+        item.setItemId(itemId);
+        item.setPrice(price);
+        item.setAvailable(available);
+        menuService.updateItem(item);
+        return ResponseEntity.ok(item);
     }
 
-    @DeleteMapping("/{itemId}")
-    public ResponseEntity<HttpStatus> deleteItem(@PathVariable Long itemId){
+    @MutationMapping()
+    public ResponseEntity<HttpStatus> deleteItem(@Argument Long itemId){
         menuService.deleteItem(itemId);
         return ResponseEntity.ok(HttpStatus.OK);
     }
--- MenuService/MenuService/src/main/java/com/prj/menu/MenuService/Entity/Item.java
+++ MenuService/MenuService/src/main/java/com/prj/menu/MenuService/Entity/Item.java
@@ -1,9 +1,16 @@
 package com.prj.menu.MenuService.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "item")
 public class Item {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long itemId;
     private String name;
     private String description;
